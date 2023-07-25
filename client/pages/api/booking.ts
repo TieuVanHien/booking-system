93cf939ec097498c1da1f8d30fa5c2972c2d2649
Van Hien <hiench2001@gmail.com>
2023-07-25 14:53:27 -0600

    fix: added access token in booking request

--- client/pages/api/booking.ts
+++ client/pages/api/booking.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
+import cookie from 'cookie';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== 'POST') {
@@ -9,36 +10,71 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     const userId = 22;
     const { title, service, duration, start, end } = req.body;
     console.log(title, service, duration, start, end, userId);
-    const accessToken = req.headers.authorization?.replace('Bearer ', '');
-    if (!accessToken) {
-      return res
-        .status(401)
-        .json({ message: 'Access token is missing or invalid' });
+    if (!req.headers.cookie) {
+      return res.status(403).json({ message: 'Unauthorized' });
     }
-    const config = {
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-        Authorization: `Bearer ${accessToken}`
-      }
-    };
-    const body = {
-      title,
-      service,
-      duration,
-      start,
-      end
-    };
     try {
-      const response = await axios.post(
-        `http://127.0.0.1:8000/api/users/${userId}/bookings/`,
+      const { refresh } = cookie.parse(req.headers.cookie);
+      const config = {
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        }
+      };
+      const body = {
+        refresh
+      };
+      const { data } = await axios.post(
+        'http://127.0.0.1:8000/api/token/refresh/',
         body,
         config
       );
-      console.log(response.data);
-      return res.status(201).json(response.data);
-    } catch (error: any) {
-      console.error('Error creating booking:', error.message);
+      if (data && data.access) {
+        const userConfig = {
+          headers: {
+            Authorization: 'Bearer ' + data.access
+          }
+        };
+        const { data: userData } = await axios.get(
+          'http://127.0.0.1:8000/api/user/',
+          userConfig
+        );
+        res.status(200).json({ user: userData, access: userData.access });
+      } else {
+        res.status(500).json({ message: 'Something went wrong' });
+      }
+      try {
+        const accessToken = data.access;
+        const config = {
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+            Authorization: `Bearer ${accessToken}`
+          }
+        };
+        const body = {
+          title,
+          service,
+          duration,
+          start,
+          end
+        };
+        try {
+          const response = await axios.post(
+            `http://127.0.0.1:8000/api/users/${userId}/bookings/`,
+            body,
+            config
+          );
+          console.log(response.data);
+          return res.status(201).json(response.data);
+        } catch (error: any) {
+          console.error('Error creating booking:', error.message);
+        }
+      } catch (error: any) {
+        console.log(error.message);
+      }
+    } catch (err) {
+      console.log(err);
     }
   } else if (req.method === 'GET') {
     const { userId } = req.query;
