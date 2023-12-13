3ab0cf65b175def0803bfa695ae760f1eccd1b19
Van Hien <hiench2001@gmail.com>
2023-12-13 12:50:35 -0700

    clean up code

--- server/main/models.py
+++ server/main/models.py
@@ -4,6 +4,7 @@ from pytz import timezone
 from datetime import datetime, timedelta
 from background_task import background
 
+
 class Booking(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
@@ -13,36 +14,32 @@ class Booking(models.Model):
     end = models.DateTimeField()
     staff = models.CharField(max_length=225, null=True)
     phone = models.CharField(max_length=17, blank=True, null=True)
-    status = models.CharField(max_length=10, blank=True, null=True) 
+    status = models.CharField(max_length=10, blank=True, null=True)
 
     def save(self, *args, **kwargs):
         calgary_tz = timezone('America/Edmonton')
         current_date = datetime.now(calgary_tz)
         event_start_mdt = self.start.astimezone(calgary_tz)
-        thirty_mins_before_event = event_start_mdt - timedelta(minutes=30)
 
         if current_date >= event_start_mdt:
             self.status = 'Inactive'
         else:
             self.status = 'Upcoming'
-        
+
         super(Booking, self).save(*args, **kwargs)
-    
-    @background(schedule=60)  
+
+    @background(schedule=60)
     def update_event_statuses():
         calgary_tz = timezone('America/Edmonton')
         current_date = datetime.now(calgary_tz)
         for event in Booking.objects.all():
             event_start_mdt = event.start.astimezone(calgary_tz)
-            thirty_mins_before_event = event_start_mdt - timedelta(minutes=30)
 
         if current_date >= event_start_mdt:
             event.status = 'Inactive'
-        elif current_date >= thirty_mins_before_event:
+        else:
             event.status = 'Upcoming'
-   
         event.save()
+
     def __str__(self):
         return self.title
-
-
