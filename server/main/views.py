84db2ee650c1991614ad2528b8277e4aa76164ad
Van Hien <hiench2001@gmail.com>
2023-12-20 14:22:44 -0700

    fix: added token and uidb for django

--- server/main/views.py
+++ server/main/views.py
@@ -150,7 +150,8 @@ class ForgotPasswordView(APIView):
             uidb64 = urlsafe_base64_encode(force_bytes(user.pk))
             token = default_token_generator.make_token(user)
 
-            reset_link = f"http://localhost:3000/resetpassword"
+            # Construct the password reset link with uidb64 and token
+            reset_link = f"http://localhost:3000/resetpassword/{uidb64}/{token}/"
             send_mail(
                 'Password Reset',
                 f'Click the following link to reset your password: {reset_link}',
